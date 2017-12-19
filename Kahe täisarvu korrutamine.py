545e3aa290d25c727ffa2050bc15af5552bb7a29
Triinupaavel <34054875+Triinupaavel@users.noreply.github.com>
2017-12-19 09:02:32 +0200

    Add files via upload
    Kahe täisarvu korrutamine

---
+++ Kahe täisarvu korrutamine.py
@@ -0,0 +1,8 @@
+#Kahe täisarvu korrutamine#
+print("Sisesta esimene korrutatav täisarv: ")
+esimene_arv = int(input())
+print("Sisesta teine korrutatav täisarv: ")
+teine_arv = int(input())
+ 
+arvude_korrutis = esimene_arv * teine_arv
+print(arvude_korrutis)

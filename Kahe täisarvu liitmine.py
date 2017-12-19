525c273e715d9a3418c4c146ca8468a783a6e92d
Triinupaavel <34054875+Triinupaavel@users.noreply.github.com>
2017-12-19 08:56:53 +0200

    Add files via upload
    Kahe täisarvu liitmine

---
+++ Kahe täisarvu liitmine.py
@@ -0,0 +1,8 @@
+#Kahe täisarvu liitmine#
+print("Sisesta esimene liidetav täisarv: ")
+esimene_arv = int(input())
+print("Sisesta teine liidetav täisarv: ")
+teine_arv = int(input())
+
+arvude_summa = esimene_arv + teine_arv
+print(arvude_summa)

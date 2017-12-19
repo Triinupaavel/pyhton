3496a6e919af147db417fb435c7b13578316ba81
Triinupaavel <34054875+Triinupaavel@users.noreply.github.com>
2017-12-19 08:56:20 +0200

    Delete vol4.py

--- vol4.py
+++
@@ -1,7 +0,0 @@
-print("Sisesta esimene liidetav täisarv: ")
-esimene_arv = int(input())
-print("Sisesta teine liidetav täisarv: ")
-teine_arv = int(input())
- 
-arvude_summa = esimene_arv + teine_arv
-print(arvude_summa)

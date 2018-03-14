52ad5c3803b995a7c3e651edc11734bf14c73b17
Triinupaavel <34054875+Triinupaavel@users.noreply.github.com>
2018-03-14 18:48:01 +0200

    Delete Kahe täisarvu liitmine.py

--- Kahe täisarvu liitmine.py
+++
@@ -1,8 +0,0 @@
-#Kahe täisarvu liitmine#
-print("Sisesta esimene liidetav täisarv: ")
-esimene_arv = int(input())
-print("Sisesta teine liidetav täisarv: ")
-teine_arv = int(input())
-
-arvude_summa = esimene_arv + teine_arv
-print(arvude_summa)

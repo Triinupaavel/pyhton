bd717f9c100714814e9f8fc30612271a45684eca
Triinupaavel <34054875+Triinupaavel@users.noreply.github.com>
2018-03-14 18:50:52 +0200

    Add files via upload

---
+++ kahe täisarvu lahutamine.py
@@ -0,0 +1,9 @@
+#Kahe täisarvu lahutamine#
+
+print("Sisesta esimene lahutatav täisarv: ")
+esimene_arv = int(input())
+print("Sisesta teine lahutatav täisarv: ")
+teine_arv = int(input())
+
+arvude_vahe = esimene_arv - teine_arv
+print(arvude_vahe)
---
+++ tere maailm.py
@@ -0,0 +1,6 @@
+#Tere maailm#
+
+print("Tere maailm!")
+print("Mis on teie nimi?")
+myName=input()
+print("Tere,"+ myName)

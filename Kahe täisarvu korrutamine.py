38c92decea1a2e14f09e4ca7ad94d2505a8e7b82
Triinupaavel <34054875+Triinupaavel@users.noreply.github.com>
2018-03-14 18:48:54 +0200

    Delete Kahe täisarvu korrutamine.py

--- Kahe täisarvu korrutamine.py
+++
@@ -1,8 +0,0 @@
-#Kahe täisarvu korrutamine#
-print("Sisesta esimene korrutatav täisarv: ")
-esimene_arv = int(input())
-print("Sisesta teine korrutatav täisarv: ")
-teine_arv = int(input())
- 
-arvude_korrutis = esimene_arv * teine_arv
-print(arvude_korrutis)
